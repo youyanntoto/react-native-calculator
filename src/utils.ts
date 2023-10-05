b9b89c80f5132f1e8a9ce63f054fe689a99fa362
youyanntoto@gmail.com <youyanntoto@gmail.com>
2023-10-05 14:23:13 +0900

    formatNumber処理を削除

--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,13 @@ export function formatNumber(
   decimalSeparator: string,
   thousandSeparator: string
 ): string {
-  const nums = num.toString().split('.')
-  const n = nums[0]
-
-  const res = n.replace(/(\d)(?=(\d\d)+\d$)/g, v => {
-    return v + thousandSeparator
-  })
-
-  return res + (nums.length > 1 ? decimalSeparator + nums[1] : '')
+  // const nums = num.toString().split('.')
+  // const n = nums[0]
+  //
+  // const res = n.replace(/(\d)(?=(\d\d)+\d$)/g, v => {
+  //   return v + thousandSeparator
+  // })
+  //
+  // return res + (nums.length > 1 ? decimalSeparator + nums[1] : '')
+  return String(num)
 }
